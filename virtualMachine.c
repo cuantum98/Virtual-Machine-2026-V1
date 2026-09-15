cae45c2d17b53fe7e0f9f98fc1f172d79f263355
Cuantu _ <stefanokapomerendino@gmail.com>
2026-09-15 19:40:54 -0300

    Cambio de la memoria principal a 1 byte en vez de 4, se añadieron las funciones para inicializar los segmentos y los registros principales, tambien se añadieron funciones para obtener una direccion fisica a partir de una logiaca y se inicio el tratamiento de las operaciones.

--- virtualMachine.c
+++ virtualMachine.c
@@ -2,10 +2,11 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-#define 1Bt 0xFF000000; // 4 mascaras constantes que sirven para sacar los 4 distintos bytes de un int32_t.
-#define 2Bt 0x00FF0000; // =
-#define 3Bt 0x0000FF00; // =
-#define 4Bt 0x000000FF; // =
+#define MAXMEMORY 16384
+
+#define 2HBt 0xFFFF0000; //Constantes para tomar los 2 bytes más significativos y los 2 menos significativos.
+#define 2LBt 0x0000FFFF; 
+
 
 //funcion que verifica si el archivo es valido, devuelve true si lo es y false si no lo es.
 // Ademas cierra el archivo en caso de que no sea valido.
@@ -26,26 +27,70 @@ bool verifyFile(FILE *file) {
             return true;
 }
 
+
 //funcion que carga el tamaño del codigo en la variable size.
-void loadCodeSize(FILE *file, int16_t *size) {
+void loadCodeSize(FILE *file, int16_t *cSize) {
     fread(size, sizeof(int16_t), 1, file);
 }
 
+
 //funcion que carga el codigo en la memoria principal.
-void loadCode(FILE *file, int16_t size, int32_t *mainMemory) {
-    int resto = size%4;
+void loadCode(FILE *file, int16_t cSize, int8_t *mainMemory) {
     int i;
-    size = size div 4;
-    if (resto>0){
-        size++;
+    for (i=0; i<cSize; i++){
+        fread(&(mainMemory[i]), sizeof(int8_t), 1, file);
     }
-    for (i=0; i<size; i++){
-        fread(&mainMemory[i], sizeof(int32_t), 1, file);
+    fclose(file);
+}
+
+
+//Funcion que inicializa la lista de segmentos.
+void setSegments(int32_t listSegments[], int16_t cSize){
+    //por ahora solo hay 2 segmentos (cs y ds) se inicializan en 0 y 1 respectivamente
+    int i=2;
+    listSegments[0]=cSize;
+    listSegments[1]=cSize; 
+    listSegments[1]=listSegments[1] << 16;     //Sumo a 0 los 2 bytes más significativos, luego hago un shif left de 2 bytes 
+    listSegments[1]+= (MAXMEMORY - cSize);     //y sumo los bytes menos significativos 
+
+    while (i < 8){
+        listSegments[i]=0xFFFFFFFF;
     }
-    mainMemory[size] = mainMemory[size] << resto*8;
+}
+
+
+//Funcion para inicializar los registros más importantes.
+void setRegisters(int32_t registers[32],int16_t cSize){
+    int CS=26, DS=27, AC=16;
+    registers[CS]=0;
+    registers[DS]=1;
+    registers[DS]= (registers[DS]<<16) +cSize;
+    registers[0]=registers[CS];
+    registers[AC]=0;
+}
+
+
+//Funcion para obtener la direccion fisica a partir de la logica.
+int getDir(int32_t logicDir){
+    int lowByte=logicDir & 2LBt;
+    int highByte=logicDir & 2HBt;
+    return (lowByte+highByte);
+}
+
+int32_t getOp(int tipoa,int tipob, int8_t *mainMemory,int ip){
+    
+}
+
+void readNextInst(int8_t *mainMemory, int32_t *registers){
+    int8_t operacion=mainMemory[registers[0]];
+    registers[1]= operacion & 0b11111;
+    registers[2]= (operacion & 0b00110000)>>4;
+    registers[3]= (operacion & 0b11000000)>>6;
 }
 
 void main(){
+    int16_t cSize=0;
     int32_t registers[32]={0}; //serian los 32 registros de 4 bytes que se piden (aunque solo se usen 17 por ahora)
-    int32_t mainMemory[4096]={0}; //seria la memoria principal de 16kib
+    int32_t listSegments[8]={0}; //serian los 8 segmentos de 4 bytes que se piden
+    int8_t mainMemory[16384]={0}; //seria la memoria principal de 16kib
 }
